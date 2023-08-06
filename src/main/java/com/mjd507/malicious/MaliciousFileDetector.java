1ce01730205e26ea18a2f5593fd152d4dc086214
Jiandong <mjd507@qq.com>
2023-08-06 12:18:35 +0800

    malicious files detector

---
+++ src/main/java/com/mjd507/malicious/MaliciousFileDetector.java
@@ -0,0 +1,20 @@
+package com.mjd507.malicious;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface MaliciousFileDetector {
+
+    default void validateDoubleExtension(MultipartFile multipartFile) {
+        String originalFilename = multipartFile.getOriginalFilename();
+        assert originalFilename != null;
+        if (originalFilename.indexOf(".") != originalFilename.lastIndexOf(".")) {
+            throw new RuntimeException("disallow filename with double extension");
+        }
+    }
+
+    boolean canUseThisDetector(MultipartFile multipartFile);
+
+    void validateFileContent(MultipartFile multipartFile) throws IOException;
+}
---
+++ src/main/java/com/mjd507/malicious/MaliciousFileDetectorService.java
@@ -0,0 +1,24 @@
+package com.mjd507.malicious;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MaliciousFileDetectorService {
+
+    private final List<MaliciousFileDetector> maliciousFileDetectorList;
+
+    public void detectFile(MultipartFile multipartFile) throws IOException {
+        MaliciousFileDetector maliciousFileDetector = maliciousFileDetectorList.stream()
+                .filter(detector -> detector.canUseThisDetector(multipartFile))
+                .findFirst().get();
+
+        maliciousFileDetector.validateDoubleExtension(multipartFile);
+        maliciousFileDetector.validateFileContent(multipartFile);
+    }
+}
