a2d642522642e662e180410fe1311aeb76542e18
Jiandong Ma <jiandong.ma.cn@gmail.com>
2025-12-12 20:33:56 +0800

    Update README.md, also disable ApplicationTest if without docker
    Signed-off-by: Jiandong Ma <jiandong.ma.cn@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-personal practice for everything unknown/new in spring
+personal practice for [everything unknown/new](https://github.com/mjd507/spring-unknown/issues/40) in spring. 
--- src/test/java/com/jiandong/ApplicationTest.java
+++ src/test/java/com/jiandong/ApplicationTest.java
@@ -2,11 +2,13 @@ package com.jiandong;
 
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 // make this test run at last.
 // due to shared middle-ware like mq are not clean.
 // (messages consumes here but verified in other test class)
 @Order(value = Integer.MAX_VALUE)
+@Testcontainers(disabledWithoutDocker = true)
 class ApplicationTest {
 
 	@Test
