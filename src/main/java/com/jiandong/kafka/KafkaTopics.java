46a5527ec7155a22ffdfab55eb0a70df5cdba31e
Jiandong Ma <jiandong.ma.cn@gmail.com>
2026-01-16 17:36:21 +0800

    Add Kafka Use case
    - for application, use kafka compose without zookeeper.
    - for test, use EmbeddedKafka.
    
    Signed-off-by: Jiandong Ma <jiandong.ma.cn@gmail.com>

---
+++ src/main/java/com/jiandong/kafka/KafkaTopics.java
@@ -0,0 +1,20 @@
+package com.jiandong.kafka;
+
+import org.apache.kafka.clients.admin.NewTopic;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopics {
+
+	@Bean
+	public NewTopic kafkaTopic1() {
+		return TopicBuilder.name("kafka-topic1")
+				.partitions(1)
+				.replicas(1)
+				.build();
+	}
+
+}
